d80950513a8437b827dcac725a09e37c540498c5
Ugandaboi <01199214@pw.edu.pl>
2026-05-11 19:46:21 +0200

    tcp klient

--- l7-1_client_tcp.c
+++ l7-1_client_tcp.c
@@ -1,12 +1,12 @@
 #include "l7_common.h"
 
-void usage(char *name) { fprintf(stderr, "USAGE: %s socket operand1 operand2 operation \n", name); }
+void usage(char *name) { fprintf(stderr, "USAGE: %s host port  operand1 operand2 operation \n", name); }
 
 void prepare_request(char ** argv, int32_t data[5]){
-    data[0] = htonl(atoi(argv[2]));
-    data[1] = htonl(atoi(argv[3]));
+    data[0] = htonl(atoi(argv[3]));
+    data[1] = htonl(atoi(argv[4]));
     data[2] = htonl(0);
-    data[3] = htonl((int32_t) (argv[4][0]));
+    data[3] = htonl((int32_t) (argv[5][0]));
     data[4] = htonl(1);
 }
 
@@ -22,12 +22,12 @@ int main(int argc, char ** argv){
     int fd;
     int32_t data[5];
 
-    if(argc != 5){
+    if(argc != 6){
         usage(argv[0]);
         return EXIT_FAILURE;
     }
 
-    fd = connect_local_socket(argv[1]);
+    fd = connect_tcp_socket(argv[1], argv[2]);
 
     prepare_request(argv, data);
 
